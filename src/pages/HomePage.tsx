7e6eb0b7658eddb085fc731debce3697741e9b22
wvacalm <w.alicarjr@gmail.com>
2023-07-14 13:55:59 -0700

    set cookie in home page (just a test)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useCookies } from "react-cookie";
 
 interface Props {
   content: string;
 }
 
 const HomePage: React.FC<Props> = ({ content }) => {
+  const [cookies, setCookie] = useCookies(['myCookie']);
+  const cookieVal = cookies.myCookie;
+  const handleClick = () => {
+    setCookie('myCookie', 'benjamin');
+  };
   return (
     <div>
       <h1>Welcome</h1>
       <p>{content}</p>
+      <button onClick={handleClick}>set cookie</button>
     </div>
   );
 };
