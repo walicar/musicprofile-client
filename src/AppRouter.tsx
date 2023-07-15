6b9bdc72337519762cc6a359bd3decc0719efc74
wvacalm <w.alicarjr@gmail.com>
2023-07-15 14:11:31 -0700

    remove Router import

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SettingsPage from "./pages/SettingsPage";
 import NotFoundPage from "./pages/NotFoundPage";
