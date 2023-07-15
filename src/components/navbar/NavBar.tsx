944f930466993983d2702b59d9f3a44808c43bbb
wvacalm <w.alicarjr@gmail.com>
2023-07-15 15:43:25 -0700

    develop navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,26 +1,12 @@
 import React from "react";
-import SignupButton from "../buttons/SignupButton";
-import LoginButton from "../buttons/LoginButton";
-import LogoutButton from "../buttons/LogoutButton";
-import { useAuth0 } from "@auth0/auth0-react";
+import NavBarButtons from "./NavBarButtons";
+import NavBarPages from "./NavBarPages";
 
 const NavBar: React.FC = () => {
-    const { isAuthenticated } = useAuth0();
-
     return (
         <div>
-            {!isAuthenticated && (
-                <>
-                    <SignupButton />
-                    <LoginButton />
-                </>
-            )}
-            <div>We should conditonally </div>
-            {isAuthenticated && (
-                <>
-                    <LogoutButton />
-                </>
-            )}
+            <NavBarPages />
+            <NavBarButtons />
         </div>
     );
 };
