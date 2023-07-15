32a0b2ebcb47230ecf228cdf72500e3e5095a58a
wvacalm <w.alicarjr@gmail.com>
2023-07-15 14:10:52 -0700

    add ErrorPage

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import LogInPage from "./pages/LogInPage";
 import SettingsPage from "./pages/SettingsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import CallbackPage from "./pages/CallbackPage";
 import ProfilePage from "./pages/ProfilePage";
+import ErrorPage from "./pages/ErrorPage";
 import AuthenticationGuard from "./components/AuthenticationGuard"
 
 const AppRouter: React.FC = () => {
@@ -15,7 +15,7 @@ const AppRouter: React.FC = () => {
       <Route path="/profile" element={<AuthenticationGuard component={ProfilePage}/>} />
       <Route path="/settings" element={<AuthenticationGuard component={SettingsPage}/>} />
       <Route path="/callback" element={<CallbackPage />} />
-      <Route path="/error" element={<NotFoundPage />} />
+      <Route path="/error" element={<ErrorPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
