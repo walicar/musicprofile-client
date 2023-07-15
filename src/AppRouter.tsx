99790484560e37652edaa215e7d31ba07d2c9483
wvacalm <w.alicarjr@gmail.com>
2023-07-15 13:21:33 -0700

    add Auth0ProviderNavigate, restructure App layer

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LogInPage from "./pages/LogInPage";
 import SettingsPage from "./pages/SettingsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import CallbackPage from "./pages/CallbackPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const AppRouter: React.FC = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage content="My Content" />}></Route>
-                <Route path="/login" element={<LogInPage content="Log In Content" />}></Route>
-                <Route path="/settings" element={<SettingsPage content="Settings Content" />}></Route>
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<HomePage content="My Content" />}></Route>
+            <Route path="/login" element={<LogInPage content="Log In Content" />}></Route>
+            <Route path="/profile" element={<ProfilePage />}/>
+            <Route path="/settings" element={<SettingsPage content="Settings Content" />}></Route>
+            <Route path="/callback" element={<CallbackPage />} />
+            <Route path="/error" element={<NotFoundPage />} />
+        </Routes>
     )
 }
 
