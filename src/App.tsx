e7b5b8f3789b90f9eefca626ff502e4654747dbf
wvacalm <w.alicarjr@gmail.com>
2023-07-15 15:33:44 -0700

    add loading to App, and restructure HomePage, ProfilePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import AppRouter from "./AppRouter";
 import { CookiesProvider } from "react-cookie";
+import { useAuth0 } from "@auth0/auth0-react";
+import PageLoader from "./components/PageLoader";
 
 function App() {
+  const {isLoading} = useAuth0()
+  if (isLoading) {
+    return (
+      <div className="page-layout">
+        <PageLoader />
+      </div>
+    );
+  }
   return (
     <CookiesProvider>
       <AppRouter />
