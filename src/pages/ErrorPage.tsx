f2b9ed14193dc22002ca88e26a3a3b502b174936
wvacalm <w.alicarjr@gmail.com>
2023-07-15 14:10:52 -0700

    add ErrorPage

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageLayout from "../components/PageLayout";
+
+const ErrorPage: React.FC = () => {
+  return (
+    <PageLayout>
+      <div>
+        <h1>Error!</h1>
+      </div>
+    </PageLayout>
+  );
+};
+
+export default ErrorPage;
