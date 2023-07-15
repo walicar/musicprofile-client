f634c4c9da8d785cf0ce4d7b7fe853dca4b6f8d6
wvacalm <w.alicarjr@gmail.com>
2023-07-15 13:21:33 -0700

    add Auth0ProviderNavigate, restructure App layer

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import Auth0ProviderNavigate from './Auth0ProviderNavigate';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Auth0Provider 
-      domain={process.env.REACT_APP_AUTH0_DOMAIN!}
-      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID!}
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <BrowserRouter>
+      <Auth0ProviderNavigate>
+        <App />
+      </Auth0ProviderNavigate>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
