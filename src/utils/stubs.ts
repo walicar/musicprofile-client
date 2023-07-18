c4c031dac80a1f2198eb181d3d24dff5461f44cf
wvacalm <w.alicarjr@gmail.com>
2023-07-17 17:43:10 -0700

    add stubs

--- src/utils/stubs.ts
+++ src/utils/stubs.ts
@@ -1,5 +1,25 @@
-const stubString = (opt?: string): string => {
+export const stubString = (opt?: string): string => {
     return opt ? `stub ${opt}` : 'stub';
 }
 
-export default stubString;
+export const stubCookie = () => {
+    return document.cookie;
+}
+
+export const stubAsyncPass = async (): Promise<any> => {
+    const promise = new Promise((resolve, _reject) => {
+        setTimeout(() => {
+            resolve("pass");
+        }, 1000);
+    });
+    return promise;
+}
+
+export const stubAsyncFail = async (): Promise<any> => {
+    const promise = new Promise((_resolve, reject) => {
+        setTimeout(() => {
+            reject("reject");
+        }, 1000);
+    });
+    return promise;
+}
