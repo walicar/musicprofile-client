6d13cd19ae5fba345fc1a28cb82355ae622c9256
wvacalm <w.alicarjr@gmail.com>
2023-07-17 18:16:59 -0700

    Profile use PageLoader

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import PageLoader from "./PageLoader";
 
 const Profile: React.FC = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return <PageLoader />;
   }
 
   if (user === undefined) {
