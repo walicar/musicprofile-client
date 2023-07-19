97520aa9446103456a83a4afe0b77a5af4469fb7
wvacalm <w.alicarjr@gmail.com>
2023-07-19 15:18:37 -0700

    add aws, reset project

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import React from "react";
-import AppRouter from "./AppRouter";
-import { CookiesProvider } from "react-cookie";
-import { useAuth0 } from "@auth0/auth0-react";
-import PageLoader from "./components/PageLoader";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const { isLoading } = useAuth0();
-  if (isLoading) {
-    return (
-      <div className="page-layout">
-        <PageLoader />
-      </div>
-    );
-  }
   return (
-    <CookiesProvider>
-      <AppRouter />
-    </CookiesProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
