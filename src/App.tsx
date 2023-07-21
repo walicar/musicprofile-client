602c627b41a76f4cb6b815c48b4b710eba33b62e
wvacalm <w.alicarjr@gmail.com>
2023-07-21 10:23:59 -0700

    implement supabase Auth on entire app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
 import React from "react";
 import "./App.css";
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DashboardPage from "./pages/DashboardPage";
 
+const PROJECT_URL = process.env.REACT_APP_SUPABASE_URL;
+const PUB_KEY = process.env.REACT_APP_SUPABASE_PUB;
+const supabase = createClient(PROJECT_URL!, PUB_KEY!);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<HomePage content={"this is home page content"} />}
-        />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }: any) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session: any) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage content={"this is home page content"} />}
+          />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
