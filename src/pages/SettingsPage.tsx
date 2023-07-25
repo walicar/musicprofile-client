5675b2260a57bc0970585ac22b1de00d324e3c39
wvacalm <w.alicarjr@gmail.com>
2023-07-25 15:03:31 -0700

    add service buttons to settings page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import PageLayout from "../components/PageLayout";
 import { useNavigate } from "react-router-dom";
+import SpotifyButton from "../components/buttons/SpotifyButton";
+import LastfmButton from "../components/buttons/LastfmButton";
 
 type Prop = {
   session?: any;
@@ -24,6 +26,8 @@ const SettingsPage: React.FC<Prop> = ({ session }) => {
       <div>
         <h1>Settings</h1>
         <h2>Welcome {session.user.email}</h2>
+        <SpotifyButton />
+        <LastfmButton />
       </div>
     </PageLayout>
   );
