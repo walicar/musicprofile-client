4e36a8d981ed146f91774199438c226d87a0841a
wvacalm <w.alicarjr@gmail.com>
2023-07-24 17:16:47 -0700

    redirect user back to home on signout

--- src/components/buttons/LogoutButton.tsx
+++ src/components/buttons/LogoutButton.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
+import { useNavigate } from "react-router-dom";
 
 const PROJECT_URL = process.env.REACT_APP_SUPABASE_URL;
 const PUB_KEY = process.env.REACT_APP_SUPABASE_PUB;
 const supabase = createClient(PROJECT_URL!, PUB_KEY!);
 
 const LogoutButton: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    supabase.auth.signOut();
+    navigate("/");
+  };
+
   return (
     <div>
-      <button onClick={() => supabase.auth.signOut()}>log out</button>
+      <button onClick={handleSignOut}>log out</button>
     </div>
   );
 };
