96556dd08c0933bd75afb739737c7500cade72b3
wvacalm <w.alicarjr@gmail.com>
2023-07-29 13:44:43 -0700

    write to tokens, update, and select

--- src/components/stubs/StubButton.tsx
+++ src/components/stubs/StubButton.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { getTokens, writeTokens } from "../../services/tokens";
+import useLocalStorageState from "use-local-storage-state";
+const ID = process.env.REACT_APP_SUPABASE_ID;
 
 const StubButton: React.FC = () => {
+  const [session, setSession]:any = useLocalStorageState(`sb-${ID}-auth-token`);
+
   const click = async () => {
     console.log("hi");
+    getTokens();
+  };
+  const otherClick = async () => {
+    if (session) {
+      const id = session.user.id;
+      writeTokens(id);
+    } else {
+      console.log("couldn't find an ID at all...")
+    }
   };
 
   return (
     <div>
       <button onClick={click}>send fetch</button>
+      <button onClick={otherClick}>write tokens</button>
     </div>
   );
 };
