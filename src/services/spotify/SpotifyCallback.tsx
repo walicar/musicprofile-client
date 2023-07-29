0d32c3c2bfa7bc8e2e0553b280d059ef493b68ec
wvacalm <w.alicarjr@gmail.com>
2023-07-29 14:08:39 -0700

    save spotify tokens into db

--- src/services/spotify/SpotifyCallback.tsx
+++ src/services/spotify/SpotifyCallback.tsx
@@ -4,6 +4,7 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { getAccessToken } from "./spotify.service";
 import PageLoader from "../../components/PageLoader";
 import useLocalStorageState from "use-local-storage-state";
+const ID = process.env.REACT_APP_SUPABASE_ID;
 
 let didInit = false;
 const SpotifyCallback: React.FC = () => {
@@ -14,6 +15,7 @@ const SpotifyCallback: React.FC = () => {
   const [accessToken, setAccessToken] = useLocalStorageState(
     "spotify-access-token",
   );
+  const [session, setSession]:any = useLocalStorageState(`sb-${ID}-auth-token`);
   const [params, setParam] = useSearchParams();
   const code = params.get("code");
   console.log("Spotify Callback");
@@ -24,14 +26,14 @@ const SpotifyCallback: React.FC = () => {
       try {
         console.log("CODE:", code);
         console.log("CODEVERIFIER", codeVerifier);
-        const token = await getAccessToken(code!, codeVerifier);
+        const token = await getAccessToken(code!, codeVerifier, session.user.id);
         console.log("TOKEN", token);
         setAccessToken(token);
       } catch (e) {
         console.log(e);
       }
     };
-    if (!didInit) {
+    if (!didInit && session) {
       // prevent infinite loop
       didInit = true;
       getToken();
