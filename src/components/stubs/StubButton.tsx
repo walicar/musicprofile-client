666f53510e0afaa6022170eb4641be725b92e09d
wvacalm <w.alicarjr@gmail.com>
2023-08-01 16:37:33 -0700

    create tokensSlice

--- src/components/stubs/StubButton.tsx
+++ src/components/stubs/StubButton.tsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useLocalStorageState from "use-local-storage-state";
 import { useSupabaseClient } from "../../contexts/SupabaseContext";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { TokenManager } from "../../database/TokenManager";
+import store from "../../app/store";
+import {
+  erase,
+  stubWrite,
+  validateTokens,
+  selectTokens,
+} from "../../features/tokens/tokensSlice";
+import { useAppSelector } from "../../app/hooks";
 
 const ID = process.env.REACT_APP_SUPABASE_ID;
 const StubButton: React.FC = () => {
   const [session, setSession]: any = useLocalStorageState(
-    `sb-${ID}-auth-token`,
+    `sb-${ID}-auth-token`
   );
+  const [stub]: any = useLocalStorageState("spotify");
+  useAppSelector(selectTokens);
   const supabase: SupabaseClient<any> = useSupabaseClient();
   const tokenManager: TokenManager = new TokenManager(
     supabase,
-    session.user.id,
+    session.user.id
   );
+
+  useEffect(() => {
+    console.log("stub changed");
+  }, [stub]);
 
   const click = async () => {
     console.log("hi");
@@ -22,15 +36,32 @@ const StubButton: React.FC = () => {
 
   const otherClick = async () => {
     if (session) {
-      const token = { spotify: "hello_there" };
-      await tokenManager.writeTokens(token);
+      // write the tokens to DB
+      const mything = { spotify: "hello_there" };
+      await tokenManager.writeTokens(mything);
     }
+  };
+
+  const dispatchRedux = () => {
+    store.dispatch(stubWrite(["spotify"]));
+  };
+
+  const dispatchRemove = () => {
+    store.dispatch(erase(["spotify"]));
+  };
+
+  const otherDispatch = () => {
+    store.dispatch(validateTokens(["spotify"]));
   };
 
   return (
     <div>
       <button onClick={click}>send fetch</button>
       <button onClick={otherClick}>write tokens</button>
+      <button onClick={dispatchRedux}>Redux write Spotify token</button>
+      <button onClick={dispatchRemove}>Redux Remove Spotify token</button>
+      <button onClick={otherDispatch}>Redux dispatch async</button>
+      {stub ? <p>I see the stub!</p> : <p>I don't see the stub</p>}
     </div>
   );
 };
---
+++ src/features/tokens/tokensSlice.ts
@@ -0,0 +1,109 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import {
+  refreshHandlerSTUB,
+  saveRefreshTokenSTUB,
+  isExpiredSTUB,
+} from "../../components/stubs/reduxTokenStubs";
+
+type Token = {
+  access_token: string;
+  expires_in: number;
+  created_at: string;
+};
+
+const emptyToken: Token = {
+  access_token: "",
+  expires_in: 0,
+  created_at: Date(),
+};
+
+type TokensState = {
+  tokens: { [key: string]: Token };
+  status: "idle" | "validating" | "refreshing" | "validated" | "failed";
+  error: string | undefined;
+};
+
+const initialState: TokensState = {
+  tokens: {},
+  status: "idle",
+  error: undefined,
+};
+
+const tokensSlice = createSlice({
+  name: "tokens",
+  initialState,
+  reducers: {
+    erase: (state, action: PayloadAction<string[]>) => {
+      action.payload.forEach((service) => {
+        state.tokens[service] = emptyToken;
+        localStorage.removeItem(service);
+        console.log("erase from tokens slice");
+      });
+    },
+    stubWrite: (state, action: PayloadAction<string[]>) => {
+      action.payload.forEach((service) => {
+        state.tokens[service] = {
+          access_token: "fart",
+          expires_in: 1,
+          created_at: Date(),
+        };
+        localStorage.setItem(service, JSON.stringify(state.tokens[service]));
+        console.log(`wrote a token for ${service}`);
+      });
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(validateTokens.pending, (state, action) => {
+        state.status = "validating";
+        console.log("validating");
+      })
+      .addCase(validateTokens.fulfilled, (state, action) => {
+        state.status = "validated";
+        state.tokens = action.payload;
+        console.log("Validated thing");
+        for (const key in state.tokens) {
+          localStorage.setItem(key, JSON.stringify(state.tokens[key]));
+        }
+      })
+      .addCase(validateTokens.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+        console.log("failure");
+      });
+  },
+});
+
+// for each service requested,
+// if their respective token has expired,
+// - get a refreshed token
+// - store the refresh_token in DB
+// - update access_token in Redux
+export const validateTokens = createAsyncThunk(
+  "tokens/validateTokens",
+  async (services: string[], { getState }) => {
+    console.log("Trying to validate tokens");
+    const state = getState() as TokensState;
+    console.log(state);
+    let refreshedTokens: { [key: string]: Token } = {};
+    services.forEach(async (service) => {
+      if (isExpiredSTUB(state.tokens[service])) {
+        const data = await refreshHandlerSTUB[service]();
+        await saveRefreshTokenSTUB(data.refresh_token);
+        const newToken: Token = {
+          access_token: data.access_token,
+          expires_in: data.expires_in,
+          created_at: Date(),
+        };
+        refreshedTokens[service] = newToken;
+      }
+    });
+    return refreshedTokens;
+  }
+);
+
+export const selectTokens = (state: TokensState) => state.tokens;
+
+export const { erase, stubWrite } = tokensSlice.actions;
+
+export default tokensSlice.reducer;
