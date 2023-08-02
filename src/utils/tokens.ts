cff5d3662119b860791cfe09efd87571c5aae7f2
wvacalm <w.alicarjr@gmail.com>
2023-08-02 16:00:33 -0700

    move validation code off into util

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -1,4 +1,6 @@
+import { saveRefreshTokenSTUB, refreshHandlerSTUB } from "../components/stubs/reduxTokenStubs";
 import { Token } from "../features/tokens/tokensSlice"
+
 const isExpired = (token: Token) => {
     const expire = new Date(token.created_at);
     expire.setSeconds(expire.getSeconds() + token.expires_in);
@@ -7,4 +9,28 @@ const isExpired = (token: Token) => {
     return now > expire;
 }
 
-export {isExpired};
+// for each service requested,
+// if their respective token has expired,
+// - get a refreshed token
+// - store the refresh_token in DB
+// - update access_token in Redux
+const validate = async (services: string[], state: any) => {
+    // TODO: find out how to type this
+    let refreshedTokens: any = {};
+    for (const service of services) {
+      if (state.tokens[service] && isExpired(state.tokens[service])) {
+        const data = await refreshHandlerSTUB[service]();
+        await saveRefreshTokenSTUB(data.refresh_token);
+        const newToken: Token = {
+          access_token: data.access_token,
+          expires_in: data.expires_in,
+          created_at: Date(),
+        };
+        const thing = { ...refreshedTokens, [service]: newToken };
+        refreshedTokens = thing;
+      }
+      return refreshedTokens;
+    }
+  };
+
+export {validate};
