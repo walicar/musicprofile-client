ee342137185ccdfbc072032b6626d935279e179a
wvacalm <w.alicarjr@gmail.com>
2023-08-02 16:00:33 -0700

    move validation code off into util

--- src/features/tokens/tokensSlice.ts
+++ src/features/tokens/tokensSlice.ts
@@ -1,10 +1,5 @@
 import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
-import {
-  refreshHandlerSTUB,
-  saveRefreshTokenSTUB,
-  isExpiredSTUB,
-} from "../../components/stubs/reduxTokenStubs";
-import { isExpired } from "../../utils/tokens";
+import { validate } from "../../utils/tokens";
 
 export type Token = {
   access_token: string;
@@ -80,35 +75,12 @@ export const validateTokens = createAsyncThunk(
   async (services: string[], { getState }) => {
     // TODO: find out how to type this
     const { tokens: state }: any = getState() as TokensState;
-    const result = await helper(services, state)
-    console.log(result);
+    const result = await validate(services, state)
+    console.log("Tokens we validated", result);
     return result;
   }
 );
 
-// for each service requested,
-// if their respective token has expired,
-// - get a refreshed token
-// - store the refresh_token in DB
-// - update access_token in Redux
-const helper = async (services: string[], state: any) => {
-  // TODO: find out how to type this
-  let refreshedTokens: any = {};
-  for (const service of services) {
-    if (state.tokens[service] && isExpired(state.tokens[service])) {
-      const data = await refreshHandlerSTUB[service]();
-      await saveRefreshTokenSTUB(data.refresh_token);
-      const newToken: Token = {
-        access_token: data.access_token,
-        expires_in: data.expires_in,
-        created_at: Date(),
-      };
-      const thing = { ...refreshedTokens, [service]: newToken };
-      refreshedTokens = thing;
-    }
-    return refreshedTokens;
-  }
-};
 
 export const selectTokens = (state: TokensState) => state.tokens;
 
