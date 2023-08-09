376c840aab702592323ddbd6f5612925b058a720
wvacalm <w.alicarjr@gmail.com>
2023-08-08 18:40:15 -0700

    add authentication tests

---
+++ __tests__/unit/NavBar.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import NavBar from "../../src/components/navbar/NavBar";
+import { BrowserRouter } from "react-router-dom";
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    render(<BrowserRouter><NavBar /></BrowserRouter>);
+});
