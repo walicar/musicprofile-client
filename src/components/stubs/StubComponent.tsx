ae1b8364218ac066baf7ad59ae50b7c9511d3900
wvacalm <w.alicarjr@gmail.com>
2023-08-09 18:50:09 -0700

    mock fetch for tests

--- src/components/stubs/StubComponent.tsx
+++ src/components/stubs/StubComponent.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 const StubComponent: React.FC = () => {
-  return <p>This is a stub</p>;
+  const [data, setData]: any = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.example.com/data")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      {data ? (
+        <ul>
+          {data.map((item: any) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
 };
 
 export default StubComponent;
