c01f3a4104272e69be8b8ec25e1cb3e973f6477d
wvacalm <w.alicarjr@gmail.com>
2023-08-09 18:50:09 -0700

    mock fetch for tests

---
+++ src/tests/unit/FetchStub.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import StubComponent from '../../components/stubs/StubComponent';
+
+
+/** 
+ * This is just a test file to experiment with mocking fetch
+ */
+
+// Mock the fetch function
+
+describe('MyComponent', () => {
+  const mockResponse = new Response(JSON.stringify([
+    { id: 1, name: "Item 1" },
+    { id: 2, name: "Item 2" },
+  ]))
+
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue(mockResponse);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('renders data from API', async () => {
+    console.log("hello?????")
+    render(<StubComponent />);
+    
+    const item1 = await screen.findByText('Item 1');
+    const item2 = await screen.findByText('Item 2');
+    
+    expect(item1).toBeInTheDocument();
+    expect(item2).toBeInTheDocument();
+  });
+});
