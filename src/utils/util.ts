704919dcb5dc1de9f821ac9c7bb24f3aeb3ada30
wvacalm <w.alicarjr@gmail.com>
2023-08-24 23:39:40 -0700

    general validate to be used inside React

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -2,4 +2,28 @@ const isEmpty = (obj: { [key: string]: any }) => {
   return Object.keys(obj).length === 0;
 };
 
-export { isEmpty };
+type TokenSetters = {
+  spotify?: any,
+  lastfm?: any,
+}
+
+const validate = async (validator: Function, services: string[], setters: TokenSetters ) => {
+  const newTokens: Tokens = await validator(services);
+  if (!isEmpty(newTokens)) {
+    for (const service in newTokens) {
+      console.log("util validate proecssing: ", service);
+      const token: Token | undefined = newTokens[service as SupportedService];
+      if (token) {
+        console.log("util validate setting");
+        setters[service as SupportedService](token);
+      }
+    }
+  }
+} 
+
+const stubMedal = (foo: Function, bar: string, setter: any) => {
+  const newInput = foo(bar);
+  setter(newInput);
+}
+
+export { isEmpty, validate, stubMedal};
