6a477c1a97601b45a230439527186369bf766ed5
wvacalm <w.alicarjr@gmail.com>
2023-08-27 10:01:44 -0700

    New LoginPage supports sign in, sign up, forgot pass

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,28 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PageLayout from "../PageLayout";
 import SignInForm from "../forms/SignInForm";
 import SignUpForm from "../forms/SignUpForm";
-import { useLocation } from "react-router-dom";
+import ForgotForm from "@components/forms/ForgotForm";
+import { useLocation, useNavigate } from "react-router-dom";
+import useLocalStorageState from "use-local-storage-state";
 
-const LoginPage: React.FC = () => {
-  const [signIn, setSignIn] = useState<boolean>(true);
+const ID = import.meta.env.VITE_SUPABASE_ID;
+const forms: {
+  [form: string]: any;
+} = {
+  signin: <SignInForm />,
+  signup: <SignUpForm />,
+  forgot: <ForgotForm />,
+};
+
+const LoginPageBeta: React.FC = () => {
+  const [session] = useLocalStorageState(`sb-${ID}-auth-token`);
+  const navigate = useNavigate();
+  const [form, setForm] = useState<string>("signin");
   const location = useLocation();
-  if (location.state?.from.pathname) {
-    console.log("HEY COOL");
-  }
+
+  useEffect(() => {
+    if (session) navigate("/dashboard");
+  });
+  if (location.state?.from.pathname) console.log("HEY COOL");
+
   return (
     <PageLayout>
-      <h1>LoginPage</h1>
-      {signIn ? <SignInForm /> : <SignUpForm />}
-      <button
-        onClick={() => {
-          setSignIn(!signIn);
-        }}
-      >
-        {signIn ? "Create an Account" : "Sign Into Your Account"}
+      <h1>Login Page</h1>
+      {forms[form]}
+      <button id={"signin"} onClick={() => setForm("signin")}>
+        Sign In
+      </button>
+      <button id={"signup"} onClick={() => setForm("signup")}>
+        Sign Up
+      </button>
+      <button id={"forgot"} onClick={() => setForm("forgot")}>
+        Forgot
       </button>
     </PageLayout>
   );
 };
 
-export default LoginPage;
+export default LoginPageBeta;
