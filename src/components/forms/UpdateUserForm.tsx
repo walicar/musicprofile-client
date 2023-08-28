08a5a2c2d3b675f52a8208ece649de0668ce7623
wvacalm <w.alicarjr@gmail.com>
2023-08-28 11:21:08 -0700

    add update account component, stubbed

---
+++ src/components/forms/UpdateUserForm.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { useSupabaseClient } from "@components/contexts/SupabaseContext";
+import { type SupabaseClient } from "@supabase/supabase-js";
+
+const UpdateUserForm: React.FC = () => {
+  const supabase: SupabaseClient<any> = useSupabaseClient();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+
+  const updateUsername = () => {
+    console.log("handle updating user");
+  };
+  const updateEmail = () => {
+    console.log("handle updating email");
+  };
+  const updatePassword = () => {
+    console.log("handle updating password");
+  };
+
+  return (
+    <>
+      <div>
+        <label htmlFor="username">Username: </label>
+        <input
+          id="username"
+          value={username}
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        <button onClick={updateUsername}>Change Username</button>
+      </div>
+      <div>
+        <label htmlFor="email">Email: </label>
+        <input
+          id="email"
+          value={email}
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <button onClick={updateEmail}>Change Email</button>
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input
+          id="password"
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <button onClick={updatePassword}>Change Password</button>
+      </div>
+    </>
+  );
+};
+
+export default UpdateUserForm;
