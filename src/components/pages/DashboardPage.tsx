f35acf422eb5511c7992601b696039f78968b6f9
wvacalm <w.alicarjr@gmail.com>
2023-08-31 13:36:55 -0700

    remove username from DashboardPage

--- src/components/pages/DashboardPage.tsx
+++ src/components/pages/DashboardPage.tsx
@@ -9,10 +9,6 @@ const ID = import.meta.env.VITE_SUPABASE_ID;
 const DashboardPage: React.FC = () => {
   const navigate = useNavigate();
   const [session]: any = useLocalStorageState(`sb-${ID}-auth-token`);
-  const username = !!session.user.user_metadata.username
-    ? session.user.user_metadata.username
-    : session.user.email;
-
   useEffect(() => {
     if (!session) {
       navigate("/login");
@@ -25,7 +21,6 @@ const DashboardPage: React.FC = () => {
   return (
     <div className="">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 min-h-[80vh]">
-        {/* <h2>Welcome {username}</h2> */}
         <div className="mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-2 gap-y-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           <div className="min-h-[80vh] -mx-4 px-1 py-2 shadow-sm ring-1 ring-gray-900/5 sm:mx-0 sm:rounded-lg sm:px-8 sm:pb-14 lg:col-span-2 lg:row-span-2 lg:row-end-2 xl:px-8 xl:pb-8 xl:pt-8">
             <TopItemsContainer />
