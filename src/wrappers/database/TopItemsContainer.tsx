488edfbf6b68e65674879a52bdb83eb2e495d3b8
wvacalm <w.alicarjr@gmail.com>
2023-08-31 09:59:06 -0700

    flex TopItemsContainer service buttons

--- src/wrappers/database/TopItemsContainer.tsx
+++ src/wrappers/database/TopItemsContainer.tsx
@@ -10,9 +10,8 @@ export default function TopItemsContainerBeta() {
 
   return (
     <>
-      {/* <h1>{service.charAt(0).toUpperCase() + service.slice(1)} Leaderboard</h1> */}
-      <div className="grid grid-cols-5 grid-rows-1">
-        <div className="pt-12 col-span-1 col-start-1 flex flex-col">
+      <div className="flex grid-cols-5 grid-rows-1">
+        <div className="flex-none pt-12 col-span-1 col-start-1 flex flex-col">
           <button
             type="button"
             className="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
@@ -31,7 +30,7 @@ export default function TopItemsContainerBeta() {
             lastfm
           </button>
         </div>
-        <div className="col-span-4">
+        <div className="flex-1">
             <TopItems type={service} />
         </div>
       </div>
