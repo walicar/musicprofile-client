e063e5036b238400a0125671f2697e3a4e8139a9
wvacalm <w.alicarjr@gmail.com>
2023-09-01 12:16:44 -0700

    update forgot page

--- src/components/pages/ForgotPage.tsx
+++ src/components/pages/ForgotPage.tsx
@@ -1,106 +1,163 @@
-import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
+import { XMarkIcon, ExclamationCircleIcon } from "@heroicons/react/20/solid";
+import InputStyles from "@styles/InputStyles";
+//
+import React, { useState, Fragment, useRef } from "react";
 import { useSupabaseClient } from "@components/contexts/SupabaseContext";
 import { SupabaseClient } from "@supabase/supabase-js";
-import { ExclamationCircleIcon } from "@heroicons/react/20/solid";
 const URL = import.meta.env.VITE_CLIENT_URL;
-
-const Styles = {
-  isValid:
-    "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
-  notValid:
-    "block w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6",
-};
 
 const ForgotPage: React.FC = () => {
   const supabase: SupabaseClient<any, "public", any> = useSupabaseClient();
+  const recipient = useRef<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [show, setShow] = useState(false);
   const [email, setEmail] = useState<string>("");
   const [isValidEmail, setIsValidEmail] = useState(true);
 
-  const isValid = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+  const isValid = () => {
+    const pattern =
+      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    return pattern.test(email);
   };
 
   const handleForgot = async (e: React.FormEvent) => {
     e.preventDefault();
-    if (email && isValid(email)) {
-      /*
-      const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: `${URL}/settings`,
-      });
-      console.log("FORGORT PASSWORD DATA: ", data);
-      console.log("FORGORT PASSWORD ERROR: ", error);
-      setMessage(`Recovery instructions sent to ${email}`);
-      */
-    } else {
+    if (!isValid()) {
       setIsValidEmail(false);
+      setErrorMessage("Please enter a valid email address.");
+      return;
     }
+    const { error } = await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: `${URL}/settings`,
+    });
+    if (error) {
+      setIsValidEmail(false);
+      setErrorMessage(error.message);
+      return;
+    }
+    recipient.current = email;
+    setShow(true);
+    setEmail("");
   };
 
   return (
-    <div className="flex flex-1 flex-col justify-center py-1 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-        Account Recovery
-        </h2>
-      </div>
-
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-[480px]">
-        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-          <form className="space-y-6" onSubmit={handleForgot}>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="relative mt-2 rounded-md shadow-sm">
-                <input
-                  value={email}
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                    setIsValidEmail(true);
-                  }}
-                  id="email"
-                  name="email"
-                  type="text"
-                  autoComplete="email"
-                  required
-                  className={isValidEmail ? Styles.isValid : Styles.notValid}
-                />
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-                  {!isValidEmail ? (
-                    <ExclamationCircleIcon
-                      className="h-5 w-5 text-red-500"
-                      aria-hidden="true"
-                    />
-                  ) : (
-                    <></>
-                  )}
+    <>
+      <div className="flex flex-1 flex-col justify-center py-1 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Account Recovery
+          </h2>
+        </div>
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-[480px]">
+          <div className="bg-white px-6 py-8 shadow sm:rounded-lg sm:px-12">
+            <form className="space-y-3" onSubmit={handleForgot}>
+              <div className="w-full sm:h-24">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email address
+                </label>
+                <div className="relative mt-2 rounded-md shadow-sm">
+                  <input
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                      setIsValidEmail(true);
+                    }}
+                    id="email"
+                    name="email"
+                    type="text"
+                    autoComplete="email"
+                    required
+                    className={
+                      isValidEmail ? InputStyles.isValid : InputStyles.notValid
+                    }
+                  />
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                    {!isValidEmail ? (
+                      <ExclamationCircleIcon
+                        className="h-5 w-5 text-red-500"
+                        aria-hidden="true"
+                      />
+                    ) : (
+                      <></>
+                    )}
+                  </div>
                 </div>
-              </div>
-              {!isValidEmail ? (
-                <p className="mt-2 text-sm text-red-600" id="email-error">
-                  Not a valid email address.
-                </p>
-              ) : (
-                <></>
-              )}
-            </div>
 
-            <div>
+                {!isValidEmail ? (
+                  <p className="mt-2 text-sm text-red-600" id="email-error">
+                    {errorMessage}
+                  </p>
+                ) : (
+                  <></>
+                )}
+              </div>
               <button
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Submit
               </button>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      <div
+        aria-live="assertive"
+        className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+      >
+        <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+          <Transition
+            show={show}
+            as={Fragment}
+            enter="transform ease-out duration-300 transition"
+            enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+            enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="p-4">
+                <div className="flex items-start">
+                  <div className="flex-shrink-0">
+                    <CheckCircleIcon
+                      className="h-6 w-6 text-green-400"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <div className="ml-3 w-0 flex-1 pt-0.5">
+                    <p className="text-sm font-medium text-gray-900">
+                      Recover your account!
+                    </p>
+                    <p className="mt-1 text-sm text-gray-500">
+                      Follow the instructions we sent to {recipient.current} to
+                      recover your account.
+                    </p>
+                  </div>
+                  <div className="ml-4 flex flex-shrink-0">
+                    <button
+                      type="button"
+                      className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      onClick={() => {
+                        setShow(false);
+                      }}
+                    >
+                      <span className="sr-only">Close</span>
+                      <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+    </>
   );
 };
 
