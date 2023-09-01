3874cba283ae3e45f075fbecf04e4e02b80f675c
wvacalm <w.alicarjr@gmail.com>
2023-09-01 16:56:29 -0700

    updating password form in progress

--- src/components/forms/UpdatePasswordForm.tsx
+++ src/components/forms/UpdatePasswordForm.tsx
@@ -1,37 +1,127 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { CheckIcon } from '@heroicons/react/24/outline'
+//
 import { SupabaseClient } from "@supabase/supabase-js";
-import { useState } from "react";
+import { Fragment, useRef, useState } from 'react'
 
 type Prop = {
   supabase: SupabaseClient<any>;
 };
 const UpdatePasswordForm: React.FC<Prop> = ({ supabase }) => {
   const [password, setPassword] = useState<string>("");
+  const [open, setOpen] = useState(false)
+  const cancelButtonRef = useRef(null)
+
   const updatePassword = async () => {
     if (!password || password.length === 0) console.log("invalid password");
     const { error } = await supabase.auth.updateUser({ password: password });
     if (error) console.log(error);
   };
+
+  const doThing = () => {
+    setOpen(true);
+  };
+
   return (
-    <div className="flex items-center flex-auto">
-      <div className="w-full">
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={password}
-          className="rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          placeholder="********"
-          onChange={(e) => setPassword(e.target.value)}
-        />
+    <>
+      <div className="flex items-center flex-auto">
+        <div className="w-full">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value="password"
+            disabled
+            className="disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+        <button
+          type="button"
+          className="font-semibold text-indigo-600 hover:text-indigo-500"
+          onClick={doThing}
+        >
+          Update
+        </button>
       </div>
-      <button
-        type="button"
-        className="font-semibold text-indigo-600 hover:text-indigo-500"
-        onClick={updatePassword}
-      >
-        Update
-      </button>
-    </div>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          initialFocus={cancelButtonRef}
+          onClose={setOpen}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                  <div>
+                    <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                      <CheckIcon
+                        className="h-6 w-6 text-green-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-5">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900"
+                      >
+                        Payment successful
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Lorem ipsum, dolor sit amet consectetur adipisicing
+                          elit. Eius aliquam laudantium explicabo pariatur iste
+                          dolorem animi vitae error totam. At sapiente aliquam
+                          accusamus facere veritatis.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+                      onClick={() => setOpen(false)}
+                    >
+                      Deactivate
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                      onClick={() => setOpen(false)}
+                      ref={cancelButtonRef}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
   );
 };
 
