104be24c2e1ca13b77c4d3880f6bd70c443ce24c
wvacalm <w.alicarjr@gmail.com>
2023-09-01 14:18:50 -0700

    update margin in account pages

--- src/components/pages/ForgotPage.tsx
+++ src/components/pages/ForgotPage.tsx
@@ -43,7 +43,7 @@ const ForgotPage: React.FC = () => {
     <>
       <div className="flex flex-1 flex-col justify-center py-1 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className="mt-3 sm:mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Account Recovery
           </h2>
         </div>
