7ef1a26d79b4aa4f15f911a3422d37747dc49d64
wvacalm <w.alicarjr@gmail.com>
2023-09-01 12:07:35 -0700

    update input styles

--- src/styles/InputStyles.ts
+++ src/styles/InputStyles.ts
@@ -1,8 +1,8 @@
 const InputStyles = {
     isValid:
-      "rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+      "rounded-md block w-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
     notValid:
-      "block w-full rounded-md border-0 py-1.5 pr-3 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6",
+      "rounded-md block w-full  border-0 py-1.5 pr-3 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6",
   };
 
 export default InputStyles
