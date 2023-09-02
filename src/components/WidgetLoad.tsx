a6ce7b3fd32c85eb91728bd992a58ccb9135776b
wvacalm <w.alicarjr@gmail.com>
2023-09-02 10:47:02 -0700

    use WidgetLoad

--- src/components/WidgetLoad.tsx
+++ src/components/WidgetLoad.tsx
@@ -1,11 +1,14 @@
 import { ArrowPathIcon } from "@heroicons/react/20/solid";
-const WidgetLoad: React.FC = () => {
+type Prop = {
+  message?: string;
+};
+const WidgetLoad: React.FC<Prop> = ({ message }) => {
   return (
     <div className="grid grid-rows-2 text-gray-400 justify-center items-center mt-10">
       <div className="flex justify-center items-center mx-auto">
         <ArrowPathIcon className="h-32 w-32" />
       </div>
-      <div>Loading...</div>
+      <div>{message ? message : "Loading..."}</div>
     </div>
   );
 };
