6309dce428bb07bcb69ab90c2928c79d18e265b5
wvacalm <w.alicarjr@gmail.com>
2023-09-02 14:28:15 -0700

    new UpdatePasswordForm userflow

--- src/components/forms/UpdatePasswordForm.tsx
+++ src/components/forms/UpdatePasswordForm.tsx
@@ -1,25 +1,68 @@
-import { Dialog, Transition } from '@headlessui/react'
-import { CheckIcon } from '@heroicons/react/24/outline'
+import { Dialog, Transition } from "@headlessui/react";
+import {
+  XMarkIcon,
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/24/outline";
+import InputStyles from "@styles/InputStyles";
 //
 import { SupabaseClient } from "@supabase/supabase-js";
-import { Fragment, useRef, useState } from 'react'
+import { Fragment, useState } from "react";
+
 
 type Prop = {
   supabase: SupabaseClient<any>;
 };
 const UpdatePasswordForm: React.FC<Prop> = ({ supabase }) => {
   const [password, setPassword] = useState<string>("");
-  const [open, setOpen] = useState(false)
-  const cancelButtonRef = useRef(null)
+  const [newPassword, setNewPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [isValidPassword, setIsValidPassword] = useState(true);
+  const [isValidNewPassword, setIsValidNewPassword] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [show, setShow] = useState(false);
 
-  const updatePassword = async () => {
-    if (!password || password.length === 0) console.log("invalid password");
-    const { error } = await supabase.auth.updateUser({ password: password });
-    if (error) console.log(error);
+  const format = (input: string) =>
+    input.charAt(0).toUpperCase() + input.slice(1);
+
+  const clearForm = () => {
+    setPassword("");
+    setNewPassword("");
+    setConfirmPassword("");
+    setErrorMessage("");
+    setIsValidPassword(true);
+    setIsValidNewPassword(true);
   };
 
-  const doThing = () => {
-    setOpen(true);
+  const handleSubmit = async (e: React.FormEvent) => {
+    setErrorMessage("");
+    setIsValidNewPassword(true);
+    setIsValidPassword(true);
+    e.preventDefault();
+    if (!password.length || !newPassword.length || !confirmPassword) {
+      setIsValidPassword(false);
+      setIsValidNewPassword(false);
+      setErrorMessage("Passwords are empty");
+      return;
+    }
+    if (newPassword != confirmPassword) {
+      setIsValidNewPassword(false);
+      setErrorMessage("Passwords do not match");
+      return;
+    }
+    const { error } = await supabase.rpc("change_user_password", {
+      current_plain_password: password,
+      new_plain_password: newPassword,
+    });
+    if (error) {
+      setIsValidPassword(false);
+      setErrorMessage(format(error.message));
+      return;
+    }
+    setOpen(false);
+    setShow(true);
+    clearForm();
   };
 
   return (
@@ -38,17 +81,69 @@ const UpdatePasswordForm: React.FC<Prop> = ({ supabase }) => {
         <button
           type="button"
           className="font-semibold text-indigo-600 hover:text-indigo-500"
-          onClick={doThing}
+          onClick={() => setOpen(true)}
         >
           Update
         </button>
+      </div>
+      <div
+        aria-live="assertive"
+        className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+      >
+        <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+          <Transition
+            show={show}
+            as={Fragment}
+            enter="transform ease-out duration-300 transition"
+            enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+            enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="p-4">
+                <div className="flex items-start">
+                  <div className="flex-shrink-0">
+                    <CheckCircleIcon
+                      className="h-6 w-6 text-green-400"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <div className="ml-3 w-0 flex-1 pt-0.5">
+                    <p className="text-sm font-medium text-gray-898">
+                      Password Updated
+                    </p>
+                    <p className="mt-1 text-sm text-gray-500">
+                      You've successfully changed your password.
+                    </p>
+                  </div>
+                  <div className="ml-4 flex flex-shrink-0">
+                    <button
+                      type="button"
+                      className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      onClick={() => {
+                        setShow(false);
+                      }}
+                    >
+                      <span className="sr-only">Close</span>
+                      <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Transition>
+        </div>
       </div>
       <Transition.Root show={open} as={Fragment}>
         <Dialog
           as="div"
           className="relative z-10"
-          initialFocus={cancelButtonRef}
-          onClose={setOpen}
+          onClose={() => {
+            setOpen(false);
+            clearForm();
+          }}
         >
           <Transition.Child
             as={Fragment}
@@ -73,47 +168,154 @@ const UpdatePasswordForm: React.FC<Prop> = ({ supabase }) => {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-1 pt-3 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-3">
                   <div>
-                    <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                      <CheckIcon
-                        className="h-6 w-6 text-green-600"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <div className="mt-3 text-center sm:mt-5">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
-                        Payment successful
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Lorem ipsum, dolor sit amet consectetur adipisicing
-                          elit. Eius aliquam laudantium explicabo pariatur iste
-                          dolorem animi vitae error totam. At sapiente aliquam
-                          accusamus facere veritatis.
-                        </p>
+                    <div className="">
+                      <div className="flex justify-between">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 mb-2 text-gray-900"
+                        >
+                          Update Password
+                        </Dialog.Title>
+                        <button
+                          type="button"
+                          className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none"
+                          onClick={() => {
+                            setOpen(false);
+                            clearForm();
+                          }}
+                        >
+                          <span className="sr-only">Close</span>
+                          <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                        </button>
+                      </div>
+                      <div className="border-t-2">
+                        <form
+                          onSubmit={handleSubmit}
+                          className="grid grid-rows-5 gap-y-5 py-3 px-7"
+                        >
+                          <div>
+                            <label
+                              htmlFor="password"
+                              className="text-sm font-semibold"
+                            >
+                              Current Password
+                            </label>
+                            <div className="sm:flex-none relative rounded-md shadow-sm">
+                              <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                value={password}
+                                autoComplete="current-password"
+                                className={
+                                  isValidPassword
+                                    ? InputStyles.isValid
+                                    : InputStyles.notValid
+                                }
+                                onChange={(e) => {
+                                  setPassword(e.target.value);
+                                  setIsValidPassword(true);
+                                }}
+                              />
+                              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                                {!isValidPassword ? (
+                                  <ExclamationCircleIcon
+                                    className="h-5 w-5 text-red-500"
+                                    aria-hidden="true"
+                                  />
+                                ) : (
+                                  <></>
+                                )}
+                              </div>
+                            </div>
+                          </div>
+                          <div className="justify-center">
+                            {!isValidPassword ? (
+                              <p
+                                className="text-sm text-red-600"
+                                id="email-error"
+                              >
+                                {errorMessage}
+                              </p>
+                            ) : (
+                              <></>
+                            )}
+                          </div>
+                          <div>
+                            <label
+                              htmlFor="newPassword"
+                              className="text-sm font-semibold"
+                            >
+                              New Password
+                            </label>
+                            <div className="sm:flex-none relative rounded-md shadow-sm">
+                              <input
+                                type="password"
+                                name="newPassword"
+                                id="newPassword"
+                                value={newPassword}
+                                autoComplete="new-password"
+                                className={InputStyles.isValid}
+                                onChange={(e) => setNewPassword(e.target.value)}
+                              />
+                            </div>
+                          </div>
+                          <div>
+                            <label
+                              htmlFor="confirmPassword"
+                              className="text-sm font-semibold"
+                            >
+                              Re-enter Password
+                            </label>
+                            <div className="sm:flex-none relative rounded-md shadow-sm">
+                              <input
+                                type="password"
+                                name="confirmPassword"
+                                id="confirmPassword"
+                                value={confirmPassword}
+                                autoComplete="new-password"
+                                className={
+                                  isValidNewPassword
+                                    ? InputStyles.isValid
+                                    : InputStyles.notValid
+                                }
+                                onChange={(e) => {
+                                  setConfirmPassword(e.target.value);
+                                  setIsValidPassword(true);
+                                }}
+                              />
+                              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                                {!isValidNewPassword ? (
+                                  <ExclamationCircleIcon
+                                    className="h-5 w-5 text-red-500"
+                                    aria-hidden="true"
+                                  />
+                                ) : (
+                                  <></>
+                                )}
+                              </div>
+                            </div>
+                          </div>
+                          <div className="justify-center">
+                            {!isValidNewPassword ? (
+                              <p
+                                className="text-sm text-red-600"
+                                id="email-error"
+                              >
+                                {errorMessage}
+                              </p>
+                            ) : (
+                              <></>
+                            )}
+                          </div>
+                          <button className="font-semibold text-indigo-600 hover:text-indigo-500">
+                            Submit
+                          </button>
+                        </form>
                       </div>
                     </div>
-                  </div>
-                  <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                    <button
-                      type="button"
-                      className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
-                      onClick={() => setOpen(false)}
-                    >
-                      Deactivate
-                    </button>
-                    <button
-                      type="button"
-                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-                      onClick={() => setOpen(false)}
-                      ref={cancelButtonRef}
-                    >
-                      Cancel
-                    </button>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
