fdf9c0a621b267db237a10afd1db2741cd536caa
wvacalm <w.alicarjr@gmail.com>
2023-09-05 09:12:09 -0700

    fix bug: Warning: Each child in a list should have a unique "key" prop.

--- src/components/WidgetContainer.tsx
+++ src/components/WidgetContainer.tsx
@@ -45,7 +45,7 @@ const WidgetContainer: React.FC = () => {
             <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
               <div className="py-1">
                 {Widgets.map((item) => (
-                  <Menu.Item>
+                  <Menu.Item key={item.id}>
                     {({ active }) => (
                       <a
                         id={item.id}
