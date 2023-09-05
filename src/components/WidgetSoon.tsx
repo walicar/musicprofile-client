1205df42d30e53ec6e1507b7315f7158e6567b0e
wvacalm <w.alicarjr@gmail.com>
2023-09-05 11:10:28 -0700

    coming soon widget

---
+++ src/components/WidgetSoon.tsx
@@ -0,0 +1,15 @@
+import { ComputerDesktopIcon } from "@heroicons/react/20/solid";
+const WidgetSoon: React.FC = () => {
+  return (
+    <div className="grid grid-rows-2 text-gray-400 justify-center items-center mt-10">
+      <div className="flex justify-center items-center mx-auto">
+        <ComputerDesktopIcon className="h-32 w-32" />
+      </div>
+      <div className="flex justify-center items-center">
+        <div>Coming soon!</div>
+      </div>
+    </div>
+  );
+};
+
+export default WidgetSoon;
