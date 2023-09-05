48d6d7c40f931525250823336d5fa8d0ba0518ab
wvacalm <w.alicarjr@gmail.com>
2023-09-05 11:06:04 -0700

    add "new" status icon

--- src/utils/statusIcons.tsx
+++ src/utils/statusIcons.tsx
@@ -2,15 +2,19 @@ import {
   ChevronUpIcon,
   ChevronDownIcon,
   MinusIcon,
+  SparklesIcon
 } from "@heroicons/react/24/outline";
-const className = "h-6 w-6";
+const className = "h-6 w-6 rounded-full ";
 const statusIcons: StatusIcons = {
   promoted: (
-    <ChevronUpIcon data-label="promoted" className={className +  " rounded-full bg-green-200 text-green-900"} />
+    <ChevronUpIcon data-label="promoted" className={className +  "bg-green-200 text-green-900"} />
   ),
-  unchanged: <MinusIcon data-label="unchanged" className={className + " rounded-full bg-gray-100"} />,
+  unchanged: <MinusIcon data-label="unchanged" className={className + "bg-gray-100"} />,
   demoted: (
-    <ChevronDownIcon data-label="demoted" className={className + " rounded-full bg-red-200 text-red-900"} />
+    <ChevronDownIcon data-label="demoted" className={className + " bg-red-200 text-red-900"} />
+  ),
+  new: (
+    <SparklesIcon data-label="new" className = {className + "bg-yellow-200 text-yellow-500"} />
   ),
 };
 
