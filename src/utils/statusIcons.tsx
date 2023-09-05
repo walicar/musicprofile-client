e106e13bbaa8ec1ffdab0b54dc559a7041521c71
wvacalm <w.alicarjr@gmail.com>
2023-09-05 11:03:45 -0700

    update status icons

--- src/utils/statusIcons.tsx
+++ src/utils/statusIcons.tsx
@@ -6,11 +6,11 @@ import {
 const className = "h-6 w-6";
 const statusIcons: StatusIcons = {
   promoted: (
-    <ChevronUpIcon data-label="promoted" className={className} color="green" />
+    <ChevronUpIcon data-label="promoted" className={className +  " rounded-full bg-green-200 text-green-900"} />
   ),
-  unchanged: <MinusIcon data-label="unchanged" className={className} />,
+  unchanged: <MinusIcon data-label="unchanged" className={className + " rounded-full bg-gray-100"} />,
   demoted: (
-    <ChevronDownIcon data-label="demoted" className={className} color="red" />
+    <ChevronDownIcon data-label="demoted" className={className + " rounded-full bg-red-200 text-red-900"} />
   ),
 };
 
