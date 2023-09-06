33992014f6b5d7cdd450f5b2903f8231fee622e3
wvacalm <w.alicarjr@gmail.com>
2023-09-06 16:54:26 -0700

    implemnt lastfm button

--- src/services/lastfm/LastfmButton.tsx
+++ src/services/lastfm/LastfmButton.tsx
@@ -1,39 +1,85 @@
 import React, { useState } from "react";
 import useLocalStorageState from "use-local-storage-state";
 import InputStyles from "@styles/InputStyles";
+import { ExclamationCircleIcon } from "@heroicons/react/20/solid";
 
 const LastfmButton: React.FC = () => {
-  const [token, setToken]: any = useLocalStorageState("lastfm-token");
+  const [token, setToken, { removeItem }]: any =
+    useLocalStorageState("lastfm-token");
   const [username, setUsername] = useState<string>("");
   const [isValidUsername, setIsValidUsername] = useState(true);
   const [errorMessage, setErrorMessage] = useState<string>("");
 
   const handleClick = () => {
+    if (!username.length) {
+      setIsValidUsername(false);
+      setErrorMessage("Please enter your Lastfm username");
+      return;
+    }
+    setToken(username);
+    setUsername("");
+  };
 
+  const disconnect = () => {
+    removeItem();
   };
 
   return (
     <>
       <div className="flex items-center justify-between gap-x-3 md:gap-x-6 w-full">
-        <div className="sm:flex-none rounded-md shadow-sm">
+        <div className="sm:flex-none relative rounded-md shadow-sm">
           <input
             type="text"
             name="lastfm_username"
             id="lastfm_username"
-            value={token ? token : ""}
-            onChange={(e) => setUsername(e.target.value)}
-            className={InputStyles.isValid}
-            placeholder="Lastfm Username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+              setIsValidUsername(true);
+            }}
+            className={
+              isValidUsername ? InputStyles.isValid : InputStyles.notValid
+            }
+            placeholder={token ? token : "Lastfm Username"}
           />
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+            {!isValidUsername ? (
+              <ExclamationCircleIcon
+                className="h-5 w-5 text-red-500"
+                aria-hidden="true"
+              />
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+        <div className="pl-3 w-full">
+          {!isValidUsername ? (
+            <p className="text-sm text-red-600" id="email-error">
+              {errorMessage}
+            </p>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
-      <button
-        type="button"
-        className="font-semibold text-indigo-600 hover:text-indigo-500"
-        onClick={() => console.log("handle Lastfm button")}
-      >
-        Connect
-      </button>
+      {token ? (
+        <button
+          type="button"
+          className="font-semibold text-indigo-600 hover:text-indigo-500"
+          onClick={disconnect}
+        >
+          Disconnect
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="font-semibold text-indigo-600 hover:text-indigo-500"
+          onClick={handleClick}
+        >
+          Connect
+        </button>
+      )}
     </>
   );
 };
