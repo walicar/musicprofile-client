1ed71dc95b24f169e8272167e8de071d1279a058
wvacalm <w.alicarjr@gmail.com>
2023-09-06 12:48:25 -0700

    fix settings page

--- src/components/forms/UpdatePasswordForm.tsx
+++ src/components/forms/UpdatePasswordForm.tsx
@@ -75,7 +75,7 @@ const UpdatePasswordForm: React.FC<Prop> = ({ supabase }) => {
             id="password"
             value="password"
             disabled
-            className="disabled:cursor-not-allowed disabled:bg-gray-50 dark:disabled:bg-slate-800 disabled:text-gray-500 dark:disabled:ring-slate-600 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:disabled:bg-slate-800 dark:disabled:ring-slate-600"
           />
         </div>
         <button
