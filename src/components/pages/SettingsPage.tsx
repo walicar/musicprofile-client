19400edc7c7a19d1c62f10bc290e271f900763d6
wvacalm <w.alicarjr@gmail.com>
2023-09-06 12:48:25 -0700

    fix settings page

--- src/components/pages/SettingsPage.tsx
+++ src/components/pages/SettingsPage.tsx
@@ -42,7 +42,7 @@ const SettingsPage: React.FC = () => {
   }
 
   return (
-    <div className="mx-auto max-w-3xl px-4 pt-4 sm:px-6 lg:px-16 shadow-sm ring-1 ring-gray-900/5 dark:ring-slate-600 ounded-md">
+    <div className="mx-auto max-w-3xl px-4 pt-4 sm:px-6 lg:px-16 shadow-sm ring-1 ring-gray-900/5 dark:ring-slate-600 rounded-md">
       <div className="mx-auto max-w-3xl py-5 space-y-10 lg:mx-0 lg:max-w-none">
         <div>
           <h2 className="text-base font-semibold leading-1 text-gray-900 dark:text-neutral-50 ">
