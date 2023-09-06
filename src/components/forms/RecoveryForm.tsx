64b4cd03ef856b0041992b26b653acc9163ad88b
wvacalm <w.alicarjr@gmail.com>
2023-09-06 12:13:34 -0700

    darkmode settings page

--- src/components/forms/RecoveryForm.tsx
+++ src/components/forms/RecoveryForm.tsx
@@ -71,7 +71,7 @@ const RecoveryForm: React.FC = () => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity dark:bg-slate-800 dark:bg-opacity-95" />
           </Transition.Child>
 
           <div className="fixed inset-0 z-10 overflow-y-auto">
@@ -85,16 +85,16 @@ const RecoveryForm: React.FC = () => {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white dark:bg-slate-900 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
                   <div>
                     <div className="mt-3 text-center sm:mt-5">
                       <Dialog.Title
                         as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
+                        className="text-base font-semibold leading-6 text-gray-900 dark:text-neutral-50"
                       >
                         Update your Password
                       </Dialog.Title>
-                      <div className="border-t-2">
+                      <div className="border-t-2 dark:border-slate-600">
                         <form
                           onSubmit={handleSubmit}
                           className="grid grid-rows-3 gap-y-5 pt-5 pb-3 px-7"
@@ -102,7 +102,7 @@ const RecoveryForm: React.FC = () => {
                           <div>
                             <label
                               htmlFor="newPassword"
-                              className="text-sm font-semibold"
+                              className="text-sm font-semibold dark:text-neutral-50"
                             >
                               New Password
                             </label>
@@ -135,7 +135,7 @@ const RecoveryForm: React.FC = () => {
                           <div>
                             <label
                               htmlFor="confirmPassword"
-                              className="text-sm font-semibold"
+                              className="text-sm font-semibold dark:text-neutral-50"
                             >
                               Re-enter Password
                             </label>
@@ -208,7 +208,7 @@ const RecoveryForm: React.FC = () => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white dark:bg-slate-900 dark:text-neutral-50 dark:ring-slate-600 shadow-lg ring-1 ring-black ring-opacity-5">
               <div className="p-4">
                 <div className="flex items-start">
                   <div className="flex-shrink-0">
@@ -221,7 +221,7 @@ const RecoveryForm: React.FC = () => {
                     <p className="text-sm font-medium text-gray-898">
                       Password Updated
                     </p>
-                    <p className="mt-1 text-sm text-gray-500">
+                    <p className="mt-1 text-sm text-gray-500 dark:text-neutral-400">
                       You've successfully changed your password.
                     </p>
                   </div>
