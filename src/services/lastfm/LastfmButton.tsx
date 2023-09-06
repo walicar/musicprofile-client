483b74ba57ca145ce0a563f7bdf9395f47e2b004
wvacalm <w.alicarjr@gmail.com>
2023-09-06 16:28:02 -0700

    basic ui for lastfm

--- src/services/lastfm/LastfmButton.tsx
+++ src/services/lastfm/LastfmButton.tsx
@@ -1,23 +1,40 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+import InputStyles from "@styles/InputStyles";
 
 const LastfmButton: React.FC = () => {
-  const navigate = useNavigate();
+  const [token, setToken]: any = useLocalStorageState("lastfm-token");
+  const [username, setUsername] = useState<string>("");
+  const [isValidUsername, setIsValidUsername] = useState(true);
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   const handleClick = () => {
-    console.log("trying to connect to LASTFM");
-    navigate("/");
+
   };
 
   return (
-    <div>
+    <>
+      <div className="flex items-center justify-between gap-x-3 md:gap-x-6 w-full">
+        <div className="sm:flex-none rounded-md shadow-sm">
+          <input
+            type="text"
+            name="lastfm_username"
+            id="lastfm_username"
+            value={token ? token : ""}
+            onChange={(e) => setUsername(e.target.value)}
+            className={InputStyles.isValid}
+            placeholder="Lastfm Username"
+          />
+        </div>
+      </div>
       <button
+        type="button"
         className="font-semibold text-indigo-600 hover:text-indigo-500"
-        onClick={handleClick}
+        onClick={() => console.log("handle Lastfm button")}
       >
         Connect
       </button>
-    </div>
+    </>
   );
 };
 
