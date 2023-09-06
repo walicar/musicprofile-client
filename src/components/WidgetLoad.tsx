66b2f4585654907b42e38fdaf00a11cc57cebe9b
wvacalm <w.alicarjr@gmail.com>
2023-09-06 12:19:17 -0700

    darkmode widget load, error, soon

--- src/components/WidgetLoad.tsx
+++ src/components/WidgetLoad.tsx
@@ -4,7 +4,7 @@ type Prop = {
 };
 const WidgetLoad: React.FC<Prop> = ({ message }) => {
   return (
-    <div className="grid grid-rows-2 text-gray-400 justify-center items-center mt-10">
+    <div className="grid grid-rows-2 text-gray-400 dark:text-slate-600   justify-center items-center mt-10">
       <div className="flex justify-center items-center mx-auto">
         <ArrowPathIcon className="h-32 w-32" />
       </div>
