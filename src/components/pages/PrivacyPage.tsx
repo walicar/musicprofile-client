ede03a33608b5bd12aae28e613961693a7bb69aa
Will Alicar <will@alicar.me>
2023-09-18 13:53:24 -0700

    update Privacy policy

--- src/components/pages/PrivacyPage.tsx
+++ src/components/pages/PrivacyPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PrivacyPage: React.FC = () => {
   return (
-    <div className="grid center-items mx-auto px-6 py-4 lg:px-8 max-w-6xl">
+    <div className="grid center-items gap-y-3 mx-auto px-6 py-4 lg:px-8 max-w-6xl">
       <h1 className="font-bold text-4xl">Privacy Policy</h1>
       <br />
       <h2 className="font-bold text-xl py-2 dark:border-slate-600 border-t">
@@ -22,14 +22,16 @@ const PrivacyPage: React.FC = () => {
 
       <p>
         We collect information so that we can provide the best possible
-        experience when you use our service. Here's a breakdown of the
-        information we collect
+        experience when you use our service, as well as for analytical purposes.
+        Here's a breakdown of the information we collect:
         <br />
-        Personal Information:
+        <br />
+        <span className="font-bold">Personal Information</span>:
         <ul className="list-disc list-inside">
           <li>Email Address</li>
         </ul>
-        Non-Personal Information:
+        <br />
+        <span className="font-bold">Non-Personal Information</span>:
         <ul className="list-disc list-inside">
           <li>Log and Usage Data (e.g., IP address, browser type, location)</li>
           <li>Device Information</li>
@@ -42,6 +44,7 @@ const PrivacyPage: React.FC = () => {
         <ul className="list-disc list-inside">
           <li>Provide a functional website</li>
           <li>Respond to user inquiries and offer support</li>
+          <li>Gain insights and optimize our service</li>
         </ul>
       </p>
       <br />
@@ -53,8 +56,7 @@ const PrivacyPage: React.FC = () => {
           <li>The right to access, correct, or delete your personal data</li>
           <li>The right to object to our use of your data</li>
           <li>
-            The right to withdraw consent if you've previously given us
-            permission to use your data
+          The right to withdraw consent if given previously
           </li>
         </ul>
       </p>
@@ -63,9 +65,31 @@ const PrivacyPage: React.FC = () => {
       <p>
         When you connect your Spotify or Last.fm account we only read and store
         the top tracks, artists, genres associated with your account to provide
-        the leaderboard feature. If you want to entirely remove Musicprofile's
-        access to your Spotify account, you may do so by navigating to the apps
-        page in your account settings.
+        the leaderboard feature. To remove Musicprofile's access to your Spotify
+        account, visit your account settings.
+      </p>
+      <br />
+      <h2 className="font-bold text-xl">6. Cookie Usage</h2>
+      <p>
+        Musicprofile utilizes cookies to personalize your website theme
+        preferences, enhancing your browsing experience.
+      </p>
+      <br />
+      <h2 className="font-bold text-xl">
+        7. Analytical Data Processing with Cloudflare
+      </h2>
+      <p>
+        We use Cloudflare, a third-party service provider, to process analytical
+        data. Cloudflare helps us analyze user interactions and behaviors to
+        improve our service. This data may include website traffic, user
+        demographics, and preferences. For more details, please refer to{" "}
+        <a
+          className="underline text-orange-500"
+          href="https://www.cloudflare.com/privacypolicy/"
+        >
+          Cloudflare's privacy policy
+        </a>
+        .
       </p>
       <br />
     </div>
