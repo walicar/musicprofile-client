da8bf87ba88f2e20fa6a47598ffb95a0006d36c3
Will Alicar <will@alicar.me>
2023-09-22 17:05:58 -0700

    homepage title too big when mobile

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -7,7 +7,7 @@ const HomePage: React.FC = () => {
           <div className="flex-5 mx-auto max-w-2xl sm:py-6 lg:py-8">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
             <div className="text-center">
-              <h1 className="text-8xl text-orange-500 font-bold pb-2">MusicProfile</h1>
+              <h1 className="text-7xl sm:text-8xl text-orange-500 font-bold pb-2">MusicProfile</h1>
               <h2 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-neutral-50 sm:text-6xl">
                 Start tracking your listening history
               </h2>
