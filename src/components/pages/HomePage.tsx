4a886e704278a6bddc44701218329fb27224bae1
Will Alicar <will@alicar.me>
2023-09-28 19:38:50 -0700

    handle weird FOUC on title

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,5 +1,9 @@
-import Spline from "@splinetool/react-spline";
+import React, { useState, Suspense } from "react";
+const Spline = React.lazy(() => import("@splinetool/react-spline"));
+import logo from "@assets/musicprofile_small.png";
+
 const HomePage: React.FC = () => {
+
   return (
     <div className="bg-white dark:bg-slate-900 ">
       <div className="relative isolate px-6 py-1 lg:px-8">
@@ -7,8 +11,15 @@ const HomePage: React.FC = () => {
           <div className="flex-5 mx-auto max-w-3xl sm:py-6 lg:py-8">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
             <div className="text-center">
-              {/* <h1 className="text-7xl sm:text-8xl text-orange-500 font-bold pb-2">MusicProfile</h1> */}
-              <Spline className="sm:-my-[75px]" scene="/musicprofile.spline" />
+              <div className="sm:h-[359px] sm:-my-[75px]">
+                <Suspense>
+                  <Spline
+                    onLoad={() => {
+                    }}
+                    scene="https://prod.spline.design/9CqLr2SapXc27SAB/scene.splinecode"
+                  />
+                </Suspense>
+              </div>
               <h2>Start tracking your listening history</h2>
               <p className="mt-auto pt-5 text-lg leading-8 text-gray-600 dark:text-neutral-400">
                 Create an account to view your top songs, artists, and genres on
